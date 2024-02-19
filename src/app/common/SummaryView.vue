9950b5dbb49d71233e249a49e36192df1e593794
John Cowen <john.cowen@konghq.com>
2024-02-19 18:26:54 +0000

    WIP
    Signed-off-by: John Cowen <john.cowen@konghq.com>

--- src/app/common/SummaryView.vue
+++ src/app/common/SummaryView.vue
@@ -2,10 +2,10 @@
   <KSlideout
     ref="slideOutRef"
     class="summary-slideout"
-    prevent-close-on-blur
+    :close-on-blur="false"
     close-button-alignment="end"
     :has-overlay="false"
-    is-visible
+    visible
     :max-width="props.width"
     offset-top="var(--app-slideout-offset-top, 0)"
     data-testid="summary"
@@ -70,28 +70,10 @@ const emit = defineEmits<{
 
   }
 }
-.summary-slideout :deep(.k-slideout-header-content) {
-  padding-right: $kui-space-80;
-  padding-left: $kui-space-80;
-}
-
-.summary-slideout :deep(.border-styles) {
-  // TODO: Remove this override once KSlideout was updated in Kongponents v9’s alpha version.
-  // Overrides the left border color to the same value KCard uses for its borders.
-  border-left-color: rgba(0, 0, 0, 0.1);
-}
-
-// Aligns the position of the close button with the summary slideout card’s content box.
-.summary-slideout :deep(.close-button-start),
-.summary-slideout :deep(.close-button-end) {
-  margin-top: $kui-space-80 !important;
-}
-
-.summary-slideout :deep(.close-button-start) {
-  margin-left: $kui-space-80 !important;
-}
-
-.summary-slideout :deep(.close-button-end) {
-  margin-right: $kui-space-80 !important;
+.summary-slideout :deep(.slideout-header) {
+  position: absolute;
+  right: 0;
+  background-color: $kui-color-background;
+  border-radius: 3px;
 }
 </style>
